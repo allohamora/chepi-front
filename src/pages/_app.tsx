f7a9f41e2d5c37c0686ce4170b12ceaaa9095778
Allohamora <herman2000@yandex.ru>
2021-09-05 13:20:57 +0300

    feat: add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,8 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <title>Chepi</title>
+
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
