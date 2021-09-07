8f8e596a904d573c9269996f6b47b1629dac25f1
Allohamora <herman2000@yandex.ru>
2021-09-07 16:44:14 +0300

    feat: add modal features to pizza card and remove the modal

--- src/components/PizzaCard/index.tsx
+++ src/components/PizzaCard/index.tsx
@@ -1,11 +1,11 @@
-import { FC, useState, MouseEvent } from 'react';
+import { FC } from 'react';
 import useTranslation from 'next-translate/useTranslation';
 import { Pizza } from 'src/services/pizza/types';
-import { Card, Collapse, Typography, Table } from 'antd';
+import { Card, Typography, Table, Image } from 'antd';
 import { useConfig } from 'src/providers/ConfigProvider';
 import { PushpinOutlined, ShoppingOutlined } from '@ant-design/icons';
 import { capitalize } from 'src/utils/string';
-import { StyledCard, StyledModal, DescriptionImage } from './style';
+import { StyledCard } from './style';
 
 const CLICK_IGNORE = 'click-ignore';
 
@@ -15,22 +15,14 @@ interface PizzaCardProps {
 }
 
 const { Meta } = Card;
-const { Panel } = Collapse;
+const { Text } = Typography;
 
 export const PizzaCard: FC<PizzaCardProps> = ({ pizza, className }) => {
-  const [visible, setVisible] = useState(false);
   const {
     config: { language },
   } = useConfig();
   const { t } = useTranslation('pizza-card');
   const { image, variants } = pizza;
-
-  const openModal = (e: MouseEvent<HTMLDivElement>) => {
-    if ((e.target as HTMLDivElement).classList.contains(CLICK_IGNORE)) return;
-
-    setVisible(true);
-  };
-  const closeModal = () => setVisible(false);
 
   const title = pizza[`${language}_title`];
   const descripton = pizza[`${language}_description`];
@@ -56,33 +48,24 @@ export const PizzaCard: FC<PizzaCardProps> = ({ pizza, className }) => {
   const data = variants.map((variant, i) => ({ key: i, ...variant }));
 
   return (
-    <>
-      <StyledCard
-        className={className}
-        cover={<img alt={title} src={image} />}
-        actions={[
-          <PushpinOutlined className={CLICK_IGNORE} key="pushin" />,
-          <ShoppingOutlined className={CLICK_IGNORE} key="shopping" onClick={() => window.open(pizza.link)} />,
-        ]}
-        onClick={openModal}
-        hoverable
-      >
-        <Meta title={title} description={descripton} />
-      </StyledCard>
-
-      <StyledModal title={title} visible={visible} onCancel={closeModal}>
-        <Collapse defaultActiveKey={[3]}>
-          <Panel header={t('image')} key={1}>
-            <DescriptionImage src={image} alt={title} />
-          </Panel>
-          <Panel header={t('description')} key={2}>
-            <Typography.Text>{descripton}</Typography.Text>
-          </Panel>
-          <Panel header={t('variants')} key={3}>
+    <StyledCard
+      className={className}
+      cover={<Image alt={title} src={image} />}
+      actions={[
+        <PushpinOutlined className={CLICK_IGNORE} key="pushin" />,
+        <ShoppingOutlined className={CLICK_IGNORE} key="shopping" onClick={() => window.open(pizza.link)} />,
+      ]}
+      hoverable
+    >
+      <Meta
+        title={title}
+        description={
+          <>
+            <Text>{descripton}</Text>
             <Table bordered columns={columns} dataSource={data} pagination={false} />
-          </Panel>
-        </Collapse>
-      </StyledModal>
-    </>
+          </>
+        }
+      />
+    </StyledCard>
   );
 };
--- src/components/PizzaCard/style.ts
+++ src/components/PizzaCard/style.ts
@@ -1,37 +1,54 @@
 import styled from '@emotion/styled';
-import { Card, Modal, Image } from 'antd';
+import { Card } from 'antd';
+import { color } from 'src/style/helpers';
 
 export const StyledCard = styled(Card)`
   display: flex;
   flex-direction: column;
-  width: 200px;
+  width: 280px;
 
   .ant-card-cover {
     position: relative;
 
-    height: 200px;
-    background-color: white;
+    height: 280px;
+    background-color: ${color('white')};
   }
 
   .ant-card-actions {
     margin-top: auto;
   }
 
-  img {
+  .ant-image {
     position: absolute;
     top: 50%;
     left: 50%;
 
     transform: translate(-50%, -50%);
   }
-`;
 
-export const StyledModal = styled(Modal)`
-  .ant-modal-footer {
-    display: none;
+  .ant-card-body,
+  .ant-card-meta,
+  .ant-card-meta-detail,
+  .ant-card-meta-description {
+    width: 100%;
+    display: flex;
+    flex-grow: 1;
   }
-`;
 
-export const DescriptionImage = styled(Image)`
-  max-width: 100px;
+  .ant-card-meta-detail {
+    flex-flow: column wrap;
+  }
+
+  .ant-card-meta-detail {
+    justify-content: space-between;
+  }
+
+  .ant-card-meta-description {
+    flex-flow: column wrap;
+    justify-content: space-between;
+  }
+
+  .ant-card-meta-title {
+    width: 100%;
+  }
 `;
