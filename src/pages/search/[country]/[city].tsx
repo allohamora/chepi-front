d43d5a9df314a3e8f277e5f95902d90339f93f14
Allohamora <herman2000@yandex.ru>
2021-09-15 13:18:05 +0300

    refactor: remove prerender default pizzas

--- src/pages/search/[country]/[city].tsx
+++ src/pages/search/[country]/[city].tsx
@@ -1,30 +1,7 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
+import { GetStaticPaths } from 'next';
 import { Search } from 'src/views/Search';
-import { QueryClient } from 'react-query';
-import { keys } from 'src/lib/react-query';
-import { getPizzas } from 'src/services/pizza';
-import { dehydrate } from 'react-query/hydration';
 import { supportedCountriesVariants } from 'src/config/country';
-import { Pizza, supportedLangs } from 'src/services/pizza/types';
-
-interface Params {
-  country: Pizza['country'];
-  city: Pizza['city'];
-}
-
-export const getStaticProps: GetStaticProps = async (context) => {
-  const queryClient = new QueryClient();
-  const { country, city } = context.params as unknown as Params;
-  const query = '';
-
-  await queryClient.prefetchQuery([keys.pizzas, query], () => getPizzas({ query, city, country }));
-
-  return {
-    props: {
-      queryState: dehydrate(queryClient),
-    },
-  };
-};
+import { supportedLangs } from 'src/services/pizza/types';
 
 export const getStaticPaths: GetStaticPaths = () => {
   const paths = supportedCountriesVariants
